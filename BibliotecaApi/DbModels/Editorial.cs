9f3833d921a8ab88cac48a6cf36c4b07a135a9c9
Engell Lopez <engelllopez59@gmail.com>
2024-04-29 14:32:06 -0600

    rest para editoriales

--- BibliotecaApi/DbModels/Editorial.cs
+++ BibliotecaApi/DbModels/Editorial.cs
@@ -5,5 +5,5 @@ public partial class Editorial
 {
     public int Id { get; set; }
     public string Nombre { get; set; }
-    public bool Estado { get; set; }
+    public bool Estado { get; set; } = true;
 }
